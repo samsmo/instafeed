1660fdaf7f4c37a82dca6186a05d6313460c97dc
SamSmo <samsmolenski@gmail.com>
2013-02-24 18:33:19 -0500

    cleaning

--- js/instagrid.js
+++ js/instagrid.js
@@ -7,13 +7,12 @@
       type: "GET",
       dataType: "jsonp",
       cache: false,
-      url: composeRequestURL(opts),
+      url: createURL(opts),
       success: function (res) {
       	formatData(that,res);
       }
     });
     function formatData(obj, data){
-      console.log(data);
       if(data.meta.code == 200){
         var ul ='<ul>';
         $.each(data.data, function(key, value){
@@ -40,14 +39,13 @@
       }
     }
   }
-  function composeRequestURL(opts){
+  function createURL(opts){
     var apiEndpoint = "https://api.instagram.com/v1", para;
     para="?access_token="+opts.accessToken;
     if(opts.count !== null){
       para += "&count="+opts.count;
     }
     apiEndpoint +="/users/"+opts.userId+"/media/recent/"+para;
-    console.log(apiEndpoint);
     return apiEndpoint;
   }
   function returnLi(size,img,link){
@@ -66,7 +64,7 @@
     tag: null,
     userId: null,
     accessToken: '372608.250f576.73e72b1a5c344f40b1006959053d02bd',
-    clientId: '250f5766d7ec4f3e9b5b0f85a4cbc4ff',
+    clientId: 'cabe0d66a06645e49ae448c2616772eb',
     image_size: "low",
     count: null
   };
